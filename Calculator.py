615106a48e2330bb82c888d531f2d90e46ca86b9
Stephen <st.98.30.03@gmail.com>
2021-07-23 20:27:42 -0400

    added events example and starting on the calculator project

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,40 @@
 import tkinter as tk
 import math as m
+
+
+
+mainWindow = tk.Tk()
+
+
+
+frame = tk.Frame(master=mainWindow)
+frame.pack()
+
+label = tk.Entry(master=frame)
+
+
+clearButton = tk.Button(master=frame, text="Clear")
+numbersOnPad = 0
+for i in range(3):
+    for j in range(3):
+        numbersOnPad += 1
+        numPadFrame = tk.Frame(master=frame, relief=tk.RAISED, borderwidth=1)
+        numPadFrame.grid(row=i, column=j)
+        numButtons = tk.Button(master=numPadFrame, text=numbersOnPad)
+        numButtons.pack(fill=tk.BOTH, expand=1)
+
+
+arithmeticButtons = tk.Frame(master=frame)
+label.grid(row=0, column=0)
+clearButton.grid(row=0, column=1)
+numPadFrame.grid(row=1, column=0, sticky="sw")
+arithmeticButtons.grid(row=2, column=0)
+
+
+
+
+print(label.get())
+
+
+
+mainWindow.mainloop()
--- Events.py
+++ Events.py
@@ -7,9 +7,13 @@ window = tk.Tk()
 # Create an event handler
 def handle_keypress(event):
     """Print the character associated to the key pressed"""
-    print(event.char)
+    print(event.char + " hi")
+
+def return_key(event):
+    print(event.char + " return key pressed")
 
 # Bind keypress event to handle_keypress()
 window.bind("<Key>", handle_keypress)
+window.bind("<Return>", return_key)
 # Run the event loop
 window.mainloop()
