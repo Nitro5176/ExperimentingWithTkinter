6bf60f69392912a6a1dc9eead2332d10beabc594
root <root@LAPTOP-M3HJMF5P.localdomain>
2021-08-31 22:46:35 -0400

    Testing the grid layout

--- Calculator.py
+++ Calculator.py
@@ -14,27 +14,33 @@ label = tk.Entry(master=frame)
 
 clearButton = tk.Button(master=frame, text="Clear")
 numbersOnPad = 0
+# container.columnconfigure(index, weight)
+# container.rowconfigure(index, weight)
 
-for i in range(3):
-    for j in range(3):
-        numbersOnPad += 1
-        numPadFrame = tk.Frame(master=frame, relief=tk.RAISED, borderwidth=1)
-        numPadFrame.grid(row=i, column=j)
-        numButtons = tk.Button(master=numPadFrame, text=numbersOnPad)
-        numButtons.pack(fill=tk.BOTH, expand=1)
-
-arithmeticButtons = tk.Frame(master=frame)
+frame.columnconfigure(0, 1)
+frame.rowconfigure(1, 2)
 
 
-label.grid()
-clearButton.grid()
-numPadFrame.grid()
-arithmeticButtons.grid()
+# for i in range(3):
+#     for j in range(3):
+#         numbersOnPad += 1
+#         numPadFrame = tk.Frame(master=frame, relief=tk.RAISED, borderwidth=1)
+#         numPadFrame.grid(row=i, column=j)
+#         numButtons = tk.Button(master=numPadFrame, text=numbersOnPad)
+#         numButtons.pack(fill=tk.BOTH, expand=1)
+
+# arithmeticButtons = tk.Frame(master=frame)
+
+
+# label.grid()
+# clearButton.grid()
+# numPadFrame.grid()
+# arithmeticButtons.grid()
 
 
 
 
-print(label.get())
+# print(label.get())
 
 
 
